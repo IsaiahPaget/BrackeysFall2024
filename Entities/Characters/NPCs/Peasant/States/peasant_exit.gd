fa135eccacba00ac1c659ab1badc14000e346b7b
Isaiah Paget <isaiahpaget@gmail.com>
2024-09-09 20:39:40 -0700

    navigation works

---
+++ Entities/Characters/NPCs/Peasant/States/peasant_exit.gd
@@ -0,0 +1,21 @@
+extends State
+class_name PeasantExit
+
+@export var peasant: CharacterBody2D
+@onready var nav_agent := %NavigationAgent2D as NavigationAgent2D
+
+var _exit: Area2D
+
+func Enter() -> void:
+	_exit = get_tree().get_first_node_in_group("Exit")
+
+
+func Physics_Update(_delta: float) -> void:
+	var dir = peasant.to_local(nav_agent.get_next_path_position()).normalized()
+	peasant.velocity = dir * peasant.move_speed
+
+func _make_path() -> void:
+	nav_agent.target_position = _exit.global_position
+
+func _on_timer_timeout() -> void:
+	_make_path()
